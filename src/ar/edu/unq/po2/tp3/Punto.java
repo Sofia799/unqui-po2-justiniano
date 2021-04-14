6851da469954b31baa6be72d78a3ff5e0f61503b
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2021-04-14 20:40:35 -0300

    Versión inicial

---
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -0,0 +1,30 @@
+package ar.edu.unq.po2.tp3;
+
+public class Punto {
+	
+	private float x;
+	private float y;
+	
+	public Punto(float x, float y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public void setX(float x) {
+		this.x = x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public void setY(float y) {
+		this.y = y;
+	}
+
+}
