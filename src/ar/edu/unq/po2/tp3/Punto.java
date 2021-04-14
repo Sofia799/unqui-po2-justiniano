6ca5b825a5f1f319dab9393e2ea0c7652d63d92c
Sofia Justiniano <sofia.agustina.justiniano@gmail.com>
2021-04-14 20:48:20 -0300

    Se refactoriza el constructor para utilizar el nuevo método que recibe ambos valores a la vez.

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -5,10 +5,14 @@ public class Punto {
 	private float x;
 	private float y;
 	
+	public void setXY(float x, float y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
 	public Punto(float x, float y) {
 		super();
-		this.x = x;
-		this.y = y;
+		this.setXY(x, y);
 	}
 	
 	public float getX() {
